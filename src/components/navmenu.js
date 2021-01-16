65fda2646c1b2139bdc67eee07086ed2bced9b60
Mathieu Tourret <pablolola@live.fr>
2021-01-16 18:17:25 +0100

    create nav menu

--- src/components/navmenu.js
+++ src/components/navmenu.js
@@ -1,9 +1,11 @@
 import './navmenu.css'
-
+import logo from '../img/7-fallen-logo.png'
 function Menu () {
     return (
         <ul className="navmenu">
-
+            <li>
+                <img src={logo}></img>
+            </li>
         </ul>
     );
 }
