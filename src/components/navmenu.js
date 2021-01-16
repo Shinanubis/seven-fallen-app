7344ca43dfb9fc2aaec0ef195d7d11a74761a925
Mathieu Tourret <pablolola@live.fr>
2021-01-16 20:04:31 +0100

    creation de la navbar

--- src/components/navmenu.js
+++ src/components/navmenu.js
@@ -3,13 +3,13 @@ import logo from '../img/7-fallen-logo.png'
 function Menu () {
     return (
         <ul className="menu">
-            <li>Test 1</li>
-            <li>Test 2</li>
+            <li className="menu__item">Test 1</li>
+            <li className="menu__item">Test 2</li>
             <li>
                 <img className="menu__logo" src={logo}></img>
             </li>
-            <li>Test 3</li>
-            <li>Test 4</li>
+            <li className="menu__item">Test 3</li>
+            <li className="menu__item">Test 4</li>
         </ul>
     );
 }
