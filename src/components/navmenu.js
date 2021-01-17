1ad805540784071f897fbaa225445d5fa86d1600
Mathieu Tourret <pablolola@live.fr>
2021-01-17 15:43:38 +0100

    test

--- src/components/navmenu.js
+++ src/components/navmenu.js
@@ -3,13 +3,17 @@ import logo from '../img/7-fallen-logo.png'
 function Menu () {
     return (
         <ul className="menu">
-            <li className="menu__item">Test 1</li>
-            <li className="menu__item">Test 2</li>
+            <li className="menu-item">
+                Version 1.0.0
+            </li>
             <li>
                 <img className="menu__logo" src={logo}></img>
             </li>
-            <li className="menu__item">Test 3</li>
-            <li className="menu__item">Test 4</li>
+            <ul className="menu__connect">
+                <li>
+                    <a href="/login">login</a>
+                </li>
+            </ul>
         </ul>
     );
 }
