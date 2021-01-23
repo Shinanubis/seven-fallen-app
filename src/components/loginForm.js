c0239e175986e0d8d4edc556f209d34fb52c5d69
Mathieu Tourret <pablolola@live.fr>
2021-01-23 21:39:09 +0100

    pg adding

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -7,8 +7,10 @@ function loginForm() {
             <h2 className="form--title">Login et mot de passe :</h2>
             <input className="form--input" id="email" name="email" type="text" placeholder="Taper votre email ..."></input>
             <input id="password" className="form--input" name="password" type="password" placeholder="Password ..."></input>
-            <Link className="form--link" to="/subscribe">inscrivez-vous</Link>
+            <label htmlFor="remember" className="remember"><input id="remember" className="remember__checkbox" type="checkbox" name="remember"/>Se souvenir de moi </label>
+            {/*<Link className="form--link" to="/subscribe">inscrivez-vous</Link>*/}           
             <button className="btn btn-success" type="button">Envoyer</button>
+
         </form>
     )
 }
