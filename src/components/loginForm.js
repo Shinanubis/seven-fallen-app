04b0f566200c23481a2693d7de56e97fecc7e886
Mathieu Tourret <pablolola@live.fr>
2021-01-24 15:38:49 +0100

    adding fetch post user

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 function loginForm() {
 
-    const handleCLick = () => {
+    const handleSubmit= () => {
         fetch('https://test-seven.site/api',{
             method: 'POST',
             mode: 'cors',
@@ -14,13 +14,13 @@ function loginForm() {
     }
 
     return (
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
             <h2 className="form--title">Login et mot de passe :</h2>
             <input className="form--input" id="email" name="email" type="text" placeholder="Taper votre email ..."></input>
             <input id="password" className="form--input" name="password" type="password" placeholder="Password ..."></input>
             <label htmlFor="remember" className="remember"><input id="remember" className="remember__checkbox" type="checkbox" name="remember"/>Se souvenir de moi </label>
             {/*<Link className="form--link" to="/subscribe">inscrivez-vous</Link>*/}           
-            <button className="btn btn-success" type="button" onClick={handleCLick}>Envoyer</button>
+            <button className="btn btn-success" type="submit">Envoyer</button>
 
         </form>
     )
