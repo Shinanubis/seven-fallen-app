0eaab0b4ac05b53197e529edd8c101aae8e3ba4d
Mathieu Tourret <pablolola@live.fr>
2021-01-25 09:37:06 +0100

    add body in fetch

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -18,7 +18,8 @@ function LoginForm() {
                 email: emailInput,
                 pwd: passwordInput
             })
-            console.log(body)
+            console.log(body.email)
+            console.log(body.pwd)
             fetch('https://test-seven.site/user',{
                 method: 'POST',
                 headers,
