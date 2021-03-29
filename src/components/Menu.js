d9edda9d1a4ad18bd25495867b09a16f20aea40b
Mathieu Tourret <pablolola@live.fr>
2021-03-29 14:17:04 +0200

    setting of the navbar icon bug on the return of the form

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {RiSettings3Fill,RiFileList2Fill} from 'react-icons/ri'
 import {GrStackOverflow} from 'react-icons/gr'
 import {HiOutlineUsers} from 'react-icons/hi'
@@ -8,25 +8,29 @@ import './Menu.css'
 
 
 function Menu (props) {
-    const {classes, logged} = props;
+    const {classes,logged} = props;
     const initialState = {
         item__1: false,
-        item__2: true,
+        item__2: false,
         item__3: false,
         item__4: false
     }
+
     const [isClicked, setIsClicked] = useState(initialState);
 
 
     const handleClick = (e) => {
            e.persist()
            const field = e.currentTarget.id
-           console.log(isClicked)
            setIsClicked(initialState)
            setIsClicked(prevstate => ({...prevstate,
                [field]: true
            }))            
     }
+
+    useEffect(() => {
+        setIsClicked({...isClicked,['item__2']:true})
+    },[])
 
     return (
         <nav className={classes}>
@@ -43,12 +47,12 @@ function Menu (props) {
                         <RiFileList2Fill className="menu__icon"/>
                     </Link>
                 </li>
-                <li onClick={handleClick} id="item__3"  className={isClicked.item__3 ? "menu__item move-up" : "menu__item"}>
+                <li id="item__3"  className={isClicked.item__3 ? "menu__item move-up" : "menu__item"} onClick={handleClick}>
                     <Link className="menu__link" to="/subscribe">
                         <GrStackOverflow className="menu__icon"/>
                     </Link>
                 </li>
-                <li onClick={handleClick} id="item__4" className={isClicked.item__4 ? "menu__item move-up" : "menu__item"}>
+                <li id="item__4" className={isClicked.item__4 ? "menu__item move-up" : "menu__item"} onClick={handleClick}>
                     <Link className="menu__link" to="/gamers">
                         <HiOutlineUsers className="menu__icon"/>
                     </Link>
