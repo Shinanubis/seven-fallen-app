be65fa08f1c373e960a7539dbf14681388e62564
Mathieu Tourret <pablolola@live.fr>
2021-03-29 17:20:33 +0200

    beginning of decks page

--- src/components/DecksList.jsx
+++ src/components/DecksList.jsx
@@ -1,16 +1,20 @@
 import React, {useContext} from 'react'
+import './DecksList.css'
 import DecksContext from '../contexts/DecksContext'
 import Deck from './Deck'
 
 const DecksList = () => {
     const data = useContext(DecksContext)
     return (
-        <>
+        <div className="deck__list">
             {
                 data.map((elmt,index) => 
                     (<Deck key={index} title={elmt.title} infos={elmt.infos}/>)
-                )}
-        </>
+                )
+                
+            }
+        <div className="deck__list-layer"></div> 
+        </div>
     )
 }
 
