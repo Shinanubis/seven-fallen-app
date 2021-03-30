7c34a485311a111968ab24b06a428e7a4ec5ce69
Mathieu Tourret <pablolola@live.fr>
2021-03-30 17:47:40 +0200

    form profil fields ok

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
+import './ProfilePage.css'
 
-const ProfilePage = () => {
+const ProfileForm = () => {
     return (
-        <h1 style={{"color":"black"}}>Hey i'm ProfilePage</h1>
+        <form className="form">
+            <fieldset>
+                <legend>Contact</legend>
+                <div className="row">
+                <div className="form__radio--group">
+                            <label className="form__radio--label" htmlFor="gender">F</label>
+                            <input className="form--radio" type="radio" name="gender" id="gender" value="F"/>
+                        </div>
+                        <div className="form__radio--group">
+                            <label className="form__radio--label" htmlFor="gender">M</label> 
+                            <input className="form--radio"type="radio" name="gender" id="gender" value="M"/>
+                        </div>
+                </div>
+                <input type="text" className="form--input" placeholder="username" disabled/>
+
+                <input type="text" className="form--input" placeholder="firstname" disabled/>
+                <input type="text" className="form--input" placeholder="lastname" disabled/>
+                <input type="email" className="form--input" placeholder="email" disabled/>   
+            </fieldset>
+            <fieldset>
+                <legend>Location</legend>
+                <input type="text" className="form--input" placeholder="coordinates" disabled/>  
+            </fieldset>
+            <fieldset>
+                <legend>Preferences</legend>
+                <input type="text" className="form--input" placeholder="username" disabled/>  
+                <input type="text" className="form--input" placeholder="username" disabled/>  
+                <input type="text" className="form--input" placeholder="username" disabled/>  
+                <input type="text" className="form--input" placeholder="username" disabled/>  
+            </fieldset>
+        </form>
     )
 }
 
-export default ProfilePage
+export default ProfileForm
