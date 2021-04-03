14e7f36d1628195d80308555b0f00522841da699
Mathieu Tourret <pablolola@live.fr>
2021-04-03 09:39:49 +0200

    some change on the files

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,7 +3,8 @@ import './ErrorPage.css'
 
 function ErrorPage() {
     return (
-            <h1 className="title" style={{"color":"black"}}>404 - Not found</h1>
+        
+            <h1 className="title">404 - Not found</h1>
     )
 }
 
