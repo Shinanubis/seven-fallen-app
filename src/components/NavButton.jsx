49dc303d7f46fd2112acb405f3203cbada723ee4
Mathieu Tourret <pablolola@live.fr>
2021-06-03 15:44:18 +0200

    form handle blur

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import {useHistory} from 'react-router-dom'
 
 const NavButton = (props) => {
-    if(!props.onClick) console.log(props);
+    if(!props.onClick) props.onClick = () => {return};
+    console.log(props)
     const {text, url, classes, onClick} = props;
     const history = useHistory();
     const handleClick = (e) => {
