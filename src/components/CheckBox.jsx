dca3ddaab0d2ce542b9b754ddfa22f5a4239cb51
Mathieu Tourret <pablolola@live.fr>
2021-06-03 16:14:31 +0200

    form handle blur

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const CheckBox = (props) => {
-    if(!props.default) props.checked = true;
-    const {classes, name, id,text, checked} = props
+    
+    const {classes, name, id,text, checked, onChange} = props
 
     return (
         <div className="row">
-          <input id={name} className={classes} type="checkbox" name={name} checked={checked}/>
+          <input id={name} className={classes} type="checkbox" name={name} onChange={onChange}/>
           <label className="form__label" htmlFor={id}>{text}</label>   
         </div>
     )
