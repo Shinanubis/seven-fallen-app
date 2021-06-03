1918f4291d892f7f3ee58a1867848c717aa21506
Mathieu Tourret <pablolola@live.fr>
2021-06-03 15:16:46 +0200

    form handle blur

--- src/pages/AddingDeckPage.jsx
+++ src/pages/AddingDeckPage.jsx
@@ -27,6 +27,7 @@ const AddingDeckPage = (props) => {
     const handleCreate = async (e) => {
         e.preventDefault();
         let response = await createUserDeck();
+        console.log(response)
     }
 
     return (
