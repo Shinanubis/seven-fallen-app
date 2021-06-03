edcc5b72c63859a6b089ae478a5925b94c3d70d3
Mathieu Tourret <pablolola@live.fr>
2021-06-03 16:28:59 +0200

    form handle create

--- src/pages/AddingDeckPage.jsx
+++ src/pages/AddingDeckPage.jsx
@@ -41,7 +41,7 @@ const AddingDeckPage = (props) => {
                 <div className="form--section">
                     <h4 className="form__section--title">Informations</h4>
                     <InputText classes="form--input" placeholder="Nom du deck" onBlur={handleBlur} value={fieldValues.deck_name}/>
-                    {console.log(fieldValues.deck_name.length)}
+                    {fieldValues.deck_name.length === 0 ?? <span className="bad__input">Bad input</span>}
                     <CheckBox id="visible" name="visibility" classes="form__checkbox" text="public" onChange={handleCheck} checked={fieldValues.visibility}/>
                 </div>
             </form>
