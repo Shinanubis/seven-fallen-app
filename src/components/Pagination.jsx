99bba40ddf5c0cfc717ed61e9e19b21ce783d18e
Mathieu Tourret <pablolola@live.fr>
2021-06-05 16:08:32 +0200

    add pagination to ajax call

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -15,9 +15,11 @@ function Pagination(props){
     return (
         <div className={containerClasses ?? "pagination__block"} >
             <span className={leftClasses ?? "pagination__left--arrow"} onClick={setPage(Number(page) + 1)}></span>
-            {listSize.map((elmt, index) => {
-                return <p key={index} className={textClasses ?? "pagination__text"} onClick={setSize(elmt)}>{elmt}</p>
-            })}
+            {
+                listSize.map((elmt, index) => {
+                    return <p key={index} className={textClasses ?? "pagination__text"} onClick={setSize(elmt)}>{elmt}</p>
+                })
+            }
             <span className={rightClasses ?? "pagination__right--arrow"} onClick={setPage(Number(page) + 1)}></span>
         </div>
     );
