d0da59f2e5fda1699f20b861268183e95cc2bbea
Mathieu Tourret <pablolola@live.fr>
2021-06-05 15:51:49 +0200

    add pagination to ajax call

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
 
 function Pagination(props){
-    const {page = '', setpage, setSize, containerClasses = '', leftClasses = '', rightClasses = '', textClasses = '', listSize = ''} = props;
+    const {page = '', setPage = () => {}, setSize = () => {}, containerClasses = '', leftClasses = '', rightClasses = '', textClasses = '', listSize = ''} = props;
     return (
         <div className={containerClasses ?? "pagination__block"} >
-            <span className={leftClasses ?? "pagination__left--arrow"} onClick={setpage(Number(page) + 1)}></span>
+            <span className={leftClasses ?? "pagination__left--arrow"} onClick={setPage(Number(page) + 1)}></span>
             {listSize.map((elmt, index) => {
                 <p key={index} className={textClasses ?? "pagination__text"} onClick={setSize(elmt)}>{elmt}</p>
             })}
-            <span className={rightClasses ?? "pagination__right--arrow"} onClick={setpage(Number(page) + 1)}></span>
+            <span className={rightClasses ?? "pagination__right--arrow"} onClick={setPage(Number(page) + 1)}></span>
         </div>
     );
 }
