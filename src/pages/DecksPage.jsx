b2465c40b6d02863ad29e2f64ae7dc99577f9ac8
Mathieu Tourret <pablolola@live.fr>
2021-06-05 18:06:38 +0200

    pagination block resizing

--- src/pages/DecksPage.jsx
+++ src/pages/DecksPage.jsx
@@ -43,10 +43,11 @@ const DecksPage = () => {
 
     useEffect(async () => {
         let response = await getUserDecks(reqOpt);
-        if(response.code === 404){
-            return;
+
+        if(response.code === 200){
+            return setDecksList(response);
         }
-        setDecksList(response);    
+           
     },[reqOpt]);
 
     useEffect(async () => {
