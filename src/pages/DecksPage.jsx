08ad0e8c21250e8b9125d9358846a449087a2f89
Mathieu Tourret <pablolola@live.fr>
2021-06-06 10:23:11 +0200

    test decksList response

--- src/pages/DecksPage.jsx
+++ src/pages/DecksPage.jsx
@@ -43,6 +43,7 @@ const DecksPage = () => {
 
     useEffect(async () => {
         let response = await getUserDecks(reqOpt);
+        console.log(decksList.message.length);
         if(response.code === 200){
             setDecksList(response);
         }else if(response.code !== 200 && decksList.message instanceof Array){
