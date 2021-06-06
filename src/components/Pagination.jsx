857e111733ae44e095d48023eb1b067cc0114424
Mathieu Tourret <pablolola@live.fr>
2021-06-06 10:55:49 +0200

    test decksList response

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -17,7 +17,7 @@ function Pagination(props){
 
     return (
         <div className={containerClasses ?? "pagination__block"} >
-            {console.log(options.page * options.size <= list.length)}
+            {console.log(options)}
             <div className={leftClasses ?? "pagination__arrow"} onClick={(e) => setPage(e, Number(options.page) + 1,options ,list )}><BsChevronLeft/></div>
                 <div className={containerTextBlockClasses ?? "pagination__text--block"}>
                 {
