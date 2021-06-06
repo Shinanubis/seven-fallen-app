2068a08bf70a2d9a3abda8f313153842c3768caf
Mathieu Tourret <pablolola@live.fr>
2021-06-06 11:24:11 +0200

    test pagination

--- src/pages/DecksPage.jsx
+++ src/pages/DecksPage.jsx
@@ -29,6 +29,7 @@ const DecksPage = () => {
 
     const handlePage = (e, newPage, options, list) => {
         e.preventDefault();
+        console.log(list.length)
         if(newPage <= 0){
             setReqOpt({...reqOpt, page: 1});
         }else if(list.length > options.size){
