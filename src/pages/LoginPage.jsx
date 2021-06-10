30e3be1eee086a1f47468d5d326d60a74d8717da
Mathieu Tourret <pablolola@live.fr>
2021-06-10 10:55:08 +0200

    change the login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -25,44 +25,22 @@ function Login(props) {
     }
 
     return (
-        <AuthenticationContext.Consumer>
-            {
-                obj => {
-                    return (
-                        <form className="form" onSubmit={e => obj.setLogin(e,history.push)}>
-                            <div className="form--section">
-                            <h2 className="form--title">Login</h2>
-                            <input className="form--input" id="email" name="email" 
-                                   type="text" placeholder="Enter your username ..." 
-                                   onChange={usernameInputChange} onBlur={usernameInputChange} 
-                                   value={usernameInput}/>
-                            <input id="password" className="form--input" name="password" 
-                                   type="password" placeholder="Enter your password ..." 
-                                   onChange={passwordInputChange} onBlur={passwordInputChange} 
-                                   value={passwordInput}/> 
-                            </div>      
-                            <button className="btn" type="submit">Envoyer</button>
-                            <Separator />
-                            <ul className="social__icons--list">
-                                <li>
-                                    <SocialButton bgcolor="#395693" url="https://test-seven.site/api/auth/facebook">
-                                        <FaFacebookF className="icons facebook__icon"/>
-                                        <span className="btn__social--text">Login facebook</span> 
-                                    </SocialButton>
-                                </li>
-                                <li>
-                                    <SocialButton bgcolor="#F7F7F7" url="https://test-seven.site/api/auth/google">
-                                        <FcGoogle className="icons google__icon" />
-                                        <span className="btn__social--text">Login Google</span>
-                                    </SocialButton>
-                                </li>
-                            </ul>
-                    </form>
-                    )
-                }
-            }
-        </AuthenticationContext.Consumer>
-    )
+                <ul className="social__icons--list">
+                    <li>
+                        <SocialButton bgcolor="#395693" url="https://test-seven.site/api/auth/facebook">
+                            <FaFacebookF className="icons facebook__icon"/>
+                            <span className="btn__social--text">Login facebook</span> 
+                        </SocialButton>
+                    </li>
+                    <li>
+                        <SocialButton bgcolor="#F7F7F7" url="https://test-seven.site/api/auth/google">
+                            <FcGoogle className="icons google__icon" />
+                            <span className="btn__social--text">Login Google</span>
+                        </SocialButton>
+                    </li>
+                </ul>
+            )
+
 }
 
 export default Login;
