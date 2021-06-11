b17d6ea4d1d48e4677636ae62acd0956a950eb34
Mathieu Tourret <pablolola@live.fr>
2021-06-11 11:53:25 +0200

    set popup styles

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -11,41 +11,43 @@ function Popup(props) {
                     <h3 className="popup__title">Filters :</h3>
 
                     {Object.keys(datas).map(title => {
-                        if(title.type === "checkbox"){
-                            console.log(title.type)
-                            return (
-                                <div className="popup__form--section">
-                                    <h4 className="popup__option--name">{title}</h4>
-                                        {datas[title].values.map(elmt => {
-                                                return (
-                                                    <div className="popup__option--container">
-                                                        <input className="popup__option--input" type="checkbox" id={elmt} name={elmt} />
-                                                        <label className="popup__option--label" htmlFor={elmt}>{elmt}</label>
-                                                    </div>
-                                                )
+                        console.log(title.type)
+                        return null;
+                        // if(title.type === "checkbox"){
+                            
+                        //     return (
+                        //         <div className="popup__form--section">
+                        //             <h4 className="popup__option--name">{title}</h4>
+                        //                 {datas[title].values.map(elmt => {
+                        //                         return (
+                        //                             <div className="popup__option--container">
+                        //                                 <input className="popup__option--input" type="checkbox" id={elmt} name={elmt} />
+                        //                                 <label className="popup__option--label" htmlFor={elmt}>{elmt}</label>
+                        //                             </div>
+                        //                         )
     
-                                        })}
-                                </div>
-                            )
-                        }
+                        //                 })}
+                        //         </div>
+                        //     )
+                        // }
 
-                        if(title.type === "radio"){
-                            console.log(title.type)
-                            return (
-                                <div className="popup__form--section">
-                                    <h4 className="popup__option--name">{title}</h4>
-                                        {datas[title].values.map(elmt => {
-                                                return (
-                                                    <div className="popup__option--container">
-                                                        <input className="popup__option--input" type="checkbox" id={elmt} name={elmt} />
-                                                        <label className="popup__option--label" htmlFor={elmt} name={title}>{elmt}</label>
-                                                    </div>
-                                                )
+                        // if(title.type === "radio"){
+                            
+                        //     return (
+                        //         <div className="popup__form--section">
+                        //             <h4 className="popup__option--name">{title}</h4>
+                        //                 {datas[title].values.map(elmt => {
+                        //                         return (
+                        //                             <div className="popup__option--container">
+                        //                                 <input className="popup__option--input" type="checkbox" id={elmt} name={elmt} />
+                        //                                 <label className="popup__option--label" htmlFor={elmt} name={title}>{elmt}</label>
+                        //                             </div>
+                        //                         )
     
-                                        })}
-                                </div>
-                            )
-                        }
+                        //                 })}
+                        //         </div>
+                        //     )
+                        //}
                     })}
                 </form>
             </div>
