526e7a6a05d6a221537a8a7371044cc5d25d7fe1
Mathieu Tourret <pablolola@live.fr>
2021-06-14 00:50:06 +0200

    set eroor from kingdom routes

--- src/pages/DecksPage.jsx
+++ src/pages/DecksPage.jsx
@@ -137,12 +137,11 @@ const DecksPage = () => {
             response = await getUserDecks(reqOpt);
         }
 
-        if(response.code === 200 ){
+        if(response.code === 200){
             setDecksList(response);
         }else if(response.code !== 200 && decksList.message instanceof Array){
             setDeleteResponse({code: response.code, message: response.message});
             setFlashState(false);
-            setReqOpt({...reqOpt, page: 1})
         }else{
             setFlashState(null);
         }
