967a16a634e276383cd26f055740366be28d4a83
Mathieu Tourret <pablolola@live.fr>
2021-06-14 01:03:16 +0200

    set eroor from kingdom routes

--- src/pages/DecksPage.jsx
+++ src/pages/DecksPage.jsx
@@ -26,7 +26,7 @@ const DecksPage = () => {
 
     const popupDatas = {
         "List by kingdoms": {
-            displayed: ['Poseidia', 'Eondra', 'Nuit sans fin', 'MetaScience', 'Temple de la lumière', 'Pureté Céleste', 'La voie'],
+            displayed: ['Poseidia', 'Eondra', 'Endless night', 'MetaScience', 'The light\'s temple', 'Celestial purity', 'The saber\'s way'],
             values: [1,2, 3, 4, 5, 6, 7],
             field_name: 'kingdoms',
             type: "checkbox",
