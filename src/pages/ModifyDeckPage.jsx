fe8200ed90a1d674eba419f431e8d65e08eaec0a
Mathieu Tourret <pablolola@live.fr>
2021-06-15 01:45:21 +0200

    create modify deck page

--- src/pages/ModifyDeckPage.jsx
+++ src/pages/ModifyDeckPage.jsx
@@ -16,7 +16,7 @@ function ModifyDeckPage(props){
             <form className="form">
                 <div className="form--section column">
                     <input className="form--input" type="text" placeholder="deck name" value={deckInfos.deck_name}/>
-                    <label className="form__label--textarea row" htmlFor="description">Description :</label>
+                    <label className="form__label--textarea row mb-1" htmlFor="description">Description :</label>
                     <textarea className="form___textarea" id="description" value={deckInfos.description}/>
                 </div>
             </form>
