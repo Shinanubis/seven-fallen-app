aeb1864e4e257254d16deefe042aefd39a1a5387
Mathieu Tourret <pablolola@live.fr>
2021-06-15 00:42:01 +0200

    create modify deck page

--- src/pages/ModifyDeckPage.jsx
+++ src/pages/ModifyDeckPage.jsx
@@ -1,10 +1,11 @@
 import Main from '../layouts/Main';
 
 function ModifyDeckPage(props){
-    
+    const id = props.location.deckProps;
     return (
         <Main classes="page__deck">
-            <p>{props.location}</p>
+            {console.log(id)}
+            <p></p>
         </Main>
     )
 }
