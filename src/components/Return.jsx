bd7f3a7e4c7c55311d2f34bb2a32f5aec7ef28e8
Mathieu Tourret <pablolola@live.fr>
2021-06-14 17:08:32 +0200

    test fetch

--- src/components/Return.jsx
+++ src/components/Return.jsx
@@ -1,19 +1,23 @@
-import {useHistory} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Redirect} from 'react-router-dom';
 import {GiReturnArrow} from 'react-icons/gi';
 
 function Return(props){
 
-    let history = useHistory();
+    const [red, setRed] = useState(false);
 
     const handleClick = function(e){
         e.preventDefault();
-        history.push('/decks');
+        setRed(true);
     }
 
     return (
-        <div className = "return__block" onClick={handleClick}>
-            <GiReturnArrow className="return__icon" />
-        </div>
+        <>
+            <div className = "return__block" onClick={handleClick}>
+                <GiReturnArrow className="return__icon" />
+                {red === true ? <Redirect to='/decks' /> : null}
+            </div>
+        </>
     );
 }
 
