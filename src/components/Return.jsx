0f7a3de4ce0516eaa7d48ee91c18aa3603dddfc5
Mathieu Tourret <pablolola@live.fr>
2021-06-14 16:49:44 +0200

    test fetch

--- src/components/Return.jsx
+++ src/components/Return.jsx
@@ -1,11 +1,16 @@
-import React, {useHistory} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import {GiReturnArrow} from 'react-icons/gi';
 
 function Return(props){
 
     let history = useHistory();
+    const handleClick = function(e){
+        e.preventDefault();
+        history.goBack();
+    }
+
     return (
-        <div className = "return__block">
+        <div className = "return__block" onClick={handleClick}>
             <GiReturnArrow className="return__icon" />
         </div>
     );
