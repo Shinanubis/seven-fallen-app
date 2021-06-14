7c6ea0dd6c996690fd5c09412a346992256cfc7a
Mathieu Tourret <pablolola@live.fr>
2021-06-14 02:57:08 +0200

    set eroor from kingdom routes

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,8 +2,6 @@ import React, {useState, useEffect} from 'react';
 import {BsChevronLeft,BsChevronRight} from 'react-icons/bs'
 
 
-let count = 0;
-
 function Pagination(props){
     const {
         list,
@@ -17,17 +15,7 @@ function Pagination(props){
         textClasses, 
         listSize = []
     } = props;
-
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        setCounter(counter + list.length);
-    },[list]);
-
-    useEffect(() => {
-        console.log(counter)
-    });
-
+    
     return (
         <div className={containerClasses ?? "pagination__block"} >
             <div className={leftClasses ?? "pagination__arrow"} onClick={(e) => setPage(e, Number(options.page) - 1, options, list)}><BsChevronLeft/></div>
