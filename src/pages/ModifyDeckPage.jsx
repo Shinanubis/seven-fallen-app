45969884a09822663dfcced886fa28a2dd953d10
Mathieu Tourret <pablolola@live.fr>
2021-06-15 13:47:32 +0200

    cheange deck deck page logic

--- src/pages/ModifyDeckPage.jsx
+++ src/pages/ModifyDeckPage.jsx
@@ -15,7 +15,6 @@ function ModifyDeckPage(props){
             <form className="form">
                 <div className="form--section column">
                     <input className="form--input" type="text" placeholder="deck name" value={deckInfos.deck_name}/>
-                    <label className="form__label--textarea row mb-1" htmlFor="description">Description :</label>
                     <textarea id="description" className="form__textarea" placeholder="description" value={deckInfos.description}/>
                 </div>
             </form>
