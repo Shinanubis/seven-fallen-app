eeec45b36c28e275dff14af0eb24697d42ea4601
Mathieu Tourret <pablolola@live.fr>
2021-06-16 13:28:09 +0200

    add events to  modify decks page

--- src/pages/ModifyDeckPage.jsx
+++ src/pages/ModifyDeckPage.jsx
@@ -27,6 +27,7 @@ function ModifyDeckPage(props){
                 setDeckInfos(prevState => {
                     let newArr = [...prevState.kingdom];
                     const index = newArr.indexOf(e.target.value);
+                    {console.log()}
                     if(index > -1){
                         newArr.splice(index, 1);
                     }
@@ -61,7 +62,6 @@ function ModifyDeckPage(props){
 
     return (
         <Main classes="page page__deck">
-            {console.log(deckInfos)}
             <form className="form" onChange={handleChange}>
                 <div className="form--section column">
                     <input id="deck_name" className="form--input mb-2" type="text" placeholder="deck name" value={deckInfos.deck_name}/>
