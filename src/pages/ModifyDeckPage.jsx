e137b75ff62b53554df411bd2a4aba6c3b3d59f6
Mathieu Tourret <pablolola@live.fr>
2021-06-16 10:24:46 +0200

    addd events to  modify decks page

--- src/pages/ModifyDeckPage.jsx
+++ src/pages/ModifyDeckPage.jsx
@@ -17,7 +17,6 @@ function ModifyDeckPage(props){
                 let newArr = prevState.kingdom === null ? [e.target.id] : [...prevState.kingdom, e.target.id];
                 const newSet = new Set(newArr);
                 newArr = Array.from(newSet);
-                e.target.checked = !e.target.checked;
                 return {
                     ...prevState,
                     kingdom: newArr
@@ -46,7 +45,6 @@ function ModifyDeckPage(props){
 
     return (
         <Main classes="page page__deck">
-            {console.log(deckInfos)}
             <form className="form" onChange={handleChange}>
                 <div className="form--section column">
                     <input id="deck_name" className="form--input mb-2" type="text" placeholder="deck name" value={deckInfos.deck_name}/>
