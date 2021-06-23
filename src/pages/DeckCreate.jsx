094ab48c9bfa495935a4e1456a2821761c2fa0ee
Mathieu Tourret <pablolola@live.fr>
2021-06-23 12:39:13 +0200

    test

--- src/pages/DeckCreate.jsx
+++ src/pages/DeckCreate.jsx
@@ -35,7 +35,7 @@ const DeckCreate = (props) => {
         let eden = await getEden(id);
         let register = await getRegister(id);
         let holybook = await getHolyBook(id);
-        const response = {};
+        let response = {};
 
         if(eden.message){
             response = {...response, eden: eden}
