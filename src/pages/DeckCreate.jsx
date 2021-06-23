0788b819709fefb0d21ad5853fb2203081dcd69a
Mathieu Tourret <pablolola@live.fr>
2021-06-23 23:56:17 +0200

    unused varialbles removed in LoginPage

--- src/pages/DeckCreate.jsx
+++ src/pages/DeckCreate.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useLayoutEffect, useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import {useParams, Link} from 'react-router-dom';
 import { GiStack } from 'react-icons/gi';
 import { AiFillDelete } from 'react-icons/ai';
