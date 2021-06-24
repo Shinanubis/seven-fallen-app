fdc10f4f4624cbb0c9ea2b6dc6eeb9895e288862
Mathieu Tourret <pablolola@live.fr>
2021-06-24 13:11:23 +0200

    add popup to subdeck page

--- src/pages/SubDeckPage.jsx
+++ src/pages/SubDeckPage.jsx
@@ -87,7 +87,7 @@ function SubDeckPage(props){
                             <div className="empty__container column justify-center">
                                 <Button text={`add cards to ${endUrl}`} onClick={handleClick}/>
                             </div>
-                            <PopupContainer classes="row justify-center align-center popup__container d-none" ref={popupRef}>
+                            <PopupContainer classes="row justify-center popup__container d-none" ref={popupRef}>
                                 <div className="popup__heading">
                                     <div className="popup__close" onClick={handleClick}>
                                         <AiFillCloseCircle />
