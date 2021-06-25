9f16000111fba8a749c755e42a9657395880c264
Mathieu Tourret <pablolola@live.fr>
2021-06-25 19:53:45 +0200

    modify form profile

--- src/api/CardsWareHouse.jsx
+++ src/api/CardsWareHouse.jsx
@@ -2,7 +2,7 @@ async function getTypesList(lang){
     let settings = {
         method: 'GET',
         headers: {
-            'API_KEY': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJzaXRlX25hbWUiOiJEZXZTb2xkaWVyIHRlc3RlcyIsImRvbWFpbiI6Imh0dHBzOi8vdGVzdC1zZXZlbi5zaXRlIn0.gF2HGtXAC5z6s-aP_AKrQ3IVFCu5MYFSeZK-wahZikA'
+            Authorization: 'API_KEY eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJzaXRlX25hbWUiOiJEZXZTb2xkaWVyIHRlc3RlcyIsImRvbWFpbiI6Imh0dHBzOi8vdGVzdC1zZXZlbi5zaXRlIn0.gF2HGtXAC5z6s-aP_AKrQ3IVFCu5MYFSeZK-wahZikA'
         }
 
     }
