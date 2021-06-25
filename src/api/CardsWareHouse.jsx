daf45fbc821b10adb1c55ad71135dca3e0cd5302
Mathieu Tourret <pablolola@live.fr>
2021-06-25 19:49:14 +0200

    modify form profile

--- src/api/CardsWareHouse.jsx
+++ src/api/CardsWareHouse.jsx
@@ -1,7 +1,10 @@
 async function getTypesList(lang){
     let settings = {
         method: 'GET',
-        Authorization: 'API_KEY eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJzaXRlX25hbWUiOiJEZXZTb2xkaWVyIHRlc3RlcyIsImRvbWFpbiI6Imh0dHBzOi8vdGVzdC1zZXZlbi5zaXRlIn0.gF2HGtXAC5z6s-aP_AKrQ3IVFCu5MYFSeZK-wahZikA'
+        headers: {
+            'Authorization': 'API_KEY eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJzaXRlX25hbWUiOiJEZXZTb2xkaWVyIHRlc3RlcyIsImRvbWFpbiI6Imh0dHBzOi8vdGVzdC1zZXZlbi5zaXRlIn0.gF2HGtXAC5z6s-aP_AKrQ3IVFCu5MYFSeZK-wahZikA'
+        }
+
     }
 
     let response = await fetch(`https://7fallen.ovh/api/rarity/all/${lang}`,settings);
