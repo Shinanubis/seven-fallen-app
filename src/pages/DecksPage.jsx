7cab738eed7edc7bef011bbfce95b4dc0885881b
Mathieu Tourret <pablolola@live.fr>
2021-06-25 21:52:57 +0200

    modify form profile

--- src/pages/DecksPage.jsx
+++ src/pages/DecksPage.jsx
@@ -184,7 +184,6 @@ const DecksPage = (props) => {
     useEffect(async () => {
         let response = await getUserDecks(reqOpt);
         setDecksList(response);
-        console.log(props)
     },[]);
 
     if(decksList.message && decksList.message instanceof Array){
