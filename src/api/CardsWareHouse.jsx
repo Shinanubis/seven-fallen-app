0ea4d97fbf8a73a03e2bdbf1cf12b389d9d40e0b
Mathieu Tourret <pablolola@live.fr>
2021-06-25 19:42:29 +0200

    modify form profile

--- src/api/CardsWareHouse.jsx
+++ src/api/CardsWareHouse.jsx
@@ -1,7 +1,8 @@
 async function getTypesList(lang){
     let settings = {
         method: 'GET',
-        Authorization: 'API_KEY eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJzaXRlX25hbWUiOiJEZXZTb2xkaWVyIHRlc3RlcyIsImRvbWFpbiI6Imh0dHBzOi8vdGVzdC1zZXZlbi5zaXRlIn0.gF2HGtXAC5z6s-aP_AKrQ3IVFCu5MYFSeZK-wahZikA'
+        Authorization: 'API_KEY eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJzaXRlX25hbWUiOiJEZXZTb2xkaWVyIHRlc3RlcyIsImRvbWFpbiI6Imh0dHBzOi8vdGVzdC1zZXZlbi5zaXRlIn0.gF2HGtXAC5z6s-aP_AKrQ3IVFCu5MYFSeZK-wahZikA',
+        mode:'cors'
     }
 
     let response = await fetch(`https://7fallen.ovh/api/capacities/all/${lang}`,settings);
