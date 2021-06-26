2a67809cc30f035ba92c8db38fb896d83837db35
Mathieu Tourret <pablolola@live.fr>
2021-06-26 20:03:42 +0200

    add delete profil feature

--- src/api/CardsWareHouse.jsx
+++ src/api/CardsWareHouse.jsx
@@ -91,7 +91,7 @@ async function getSubdeckCards(options){
         }
     }
     let url = new URL(`https://api.7fallen.ovh/api/cards/all/${options.lang.toUpperCase()}?name=x1&page=1&card_count=10`);
-    let response = await fetch(`https://api.7fallen.ovh/api/cards/all/${options.lang.toUpperCase()}?classes=[10]&page=1&card_count=10`,settings);
+    let response = await fetch(`https://api.7fallen.ovh/api/cards/all/${options.lang.toUpperCase()}?capacities=[2,17]&page=1&card_count=10`,settings);
     let datas = await response.json();
     return datas;
 }
