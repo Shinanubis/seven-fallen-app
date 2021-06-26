6c09e25cf66208b551a8f56c7014b51b1e09b066
Mathieu Tourret <pablolola@live.fr>
2021-06-26 13:20:58 +0200

    add delete profil feature

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -293,8 +293,8 @@ const ProfileForm = () => {
                                    value={userInfos.message.email}
                             />
                         </div>
-                        <Button classes="btn" text="update" onClick={handleClick}/>
-                        <Button classes="btn" text="delete" onClick={handleDeleteClick}/>  
+                        <Button classes="btn" text="update" bgcolor='#3be73b' onClick={handleClick}/>
+                        <Button classes="btn" text="delete" bgcolor='#ff5d5d' onClick={handleDeleteClick}/>  
                 </form>
             </Main>
     )
