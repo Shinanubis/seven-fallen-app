3d954285d06757431ea6bbdc129f80b0b3c75a44
Mathieu Tourret <pablolola@live.fr>
2021-06-27 16:25:36 +0200

    log type

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -221,6 +221,10 @@ const ProfileForm = () => {
                 <PopupContainer ref={avatarPopup} classes="popup__container d-none">
                     <label htmlFor="avatar">Choose your avatar :</label>
                     <input  id="avatar" name="avatar" type="file" accept="image/png, image/jpeg"/>
+                    <div className="row justify-between w-80">
+                        <Button text="Ok" color="#3be73b"/>
+                        <Button text="Cancel" color="#ff5d5d"/>
+                    </div>
                 </PopupContainer>
                 <form className="form" onChange={handleChange}>
                         <div className="profile__heading mb-4" onClick={handleAvatarClick}>
