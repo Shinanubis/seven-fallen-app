89254d8e8432c16e45eac8ab11dd191e4df6e41b
Mathieu Tourret <pablolola@live.fr>
2021-06-28 20:41:27 +0200

    test onChange for file upload

--- src/api/Profile.jsx
+++ src/api/Profile.jsx
@@ -39,6 +39,8 @@ async function getAvatar(){
     }
 
     let response = await fetch('https://test-seven.site/api/profile/avatar', settings);
+    let datas = await response.json();
+    return datas;
  
 }
 
