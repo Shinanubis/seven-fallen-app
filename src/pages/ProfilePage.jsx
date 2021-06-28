929b2cd52c830451aae432d2648c95fe6707461b
Mathieu Tourret <pablolola@live.fr>
2021-06-28 19:39:26 +0200

    test onChange for file upload

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -180,7 +180,7 @@ const ProfileForm = () => {
                 if(e.target.files[0].size > 200000){
                     alert("file too big");
                 }
-                console.log(e.target.files[0] === "image/jpeg")
+                console.log(e.target.files[0].type === "image/jpeg")
                 if(e.target.files[0].type !== "image/png"){
                     alert("bad file type should be png or jpg");
                 }
