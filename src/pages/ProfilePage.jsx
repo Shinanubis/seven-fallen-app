3830c75198a742941f35e40cd9d2eb324abfecfe
Mathieu Tourret <pablolola@live.fr>
2021-07-01 13:13:12 +0200

    test

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -262,7 +262,7 @@ const ProfileForm = (props) => {
                 <form className="form" onChange={handleChange}>
                         <label className="form__label--avatar mb-4" htmlFor="avatar">
                             <div className="profile__heading" onClick={handleAvatarClick}>
-                                {avatar.message && <img className="profile__avatar"
+                                {avatar.message.avatar && <img className="profile__avatar"
                                      ref={avatarImage}
                                      src={avatar.message.avatar ? avatar.message.avatar : avatar.message.avatar} 
                                      alt="avatar"
