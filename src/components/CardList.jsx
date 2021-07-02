975ef84132260728f4296f3277133b977ba7226f
Mathieu Tourret <pablolola@live.fr>
2021-07-02 14:15:55 +0200

    test

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 const List = (props) => {
     const {data} = props;
     
-    return (
+    return data instanceof Array && data.length > 0 ? (
         <ul className="layout layout__3 mb-5">
             {
                 data.map(elmt => {
@@ -13,6 +13,12 @@ const List = (props) => {
                     )
                 }
             )}
+        </ul>
+    )
+    :
+    (
+        <ul>
+
         </ul>
     )
 }
