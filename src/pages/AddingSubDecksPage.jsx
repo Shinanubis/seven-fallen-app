dd9f17b487a3bfe9392a080ba7bc0882f103e017
Mathieu Tourret <pablolola@live.fr>
2021-07-03 13:29:45 +0200

    test fetch

--- src/pages/AddingSubDecksPage.jsx
+++ src/pages/AddingSubDecksPage.jsx
@@ -14,8 +14,8 @@ function AddingSubDecksCardsPage(props) {
     });
 
     useEffect(async () => {
-        let response = await getEdenCards(1,250,'FR');
-        console.log(response.status)
+        let response = await getEdenCards(1,250,'FR').then(response => console.log(response.status));
+        
 
     },[]);
 
