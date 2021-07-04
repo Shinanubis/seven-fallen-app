aba41cb714cbd46f2a95d039b8bf4e744fdd983c
Mathieu Tourret <pablolola@live.fr>
2021-07-04 21:30:08 +0200

    change deck form css

--- src/pages/ModifyDeckPage.jsx
+++ src/pages/ModifyDeckPage.jsx
@@ -126,11 +126,12 @@ function ModifyDeckPage(props){
     const handleAppearForm = (e) => {
         e.preventDefault();
         setFormOpen(!formOpen);
-        formRef.current.scrollIntoView({
-            behavior: "smooth",
-            block: "end"
-        })
-
+        setTimeout(() => {
+            formRef.current.scrollIntoView({
+                behavior: "smooth",
+                block: "end"
+            })
+        }, 100)
     }
 
     useEffect(() => {
