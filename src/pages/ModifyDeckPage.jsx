d16798f019b9affc2bd20b87033da207e9658268
Mathieu Tourret <pablolola@live.fr>
2021-07-04 21:12:48 +0200

    change deck form css

--- src/pages/ModifyDeckPage.jsx
+++ src/pages/ModifyDeckPage.jsx
@@ -127,7 +127,7 @@ function ModifyDeckPage(props){
         setFormOpen(!formOpen);
         window.document.body.scrollIntoView({
             behavior: 'smooth',
-            block: "end"
+            block: "start"
         });
     }
 
