0a40de916ba5b2a4623d44ed0f02c81d44fc4902
Mathieu Tourret <pablolola@live.fr>
2021-07-05 14:28:33 +0200

    start hoc for infinite scroll

--- src/components/InfiniteList.jsx
+++ src/components/InfiniteList.jsx
@@ -10,7 +10,7 @@ function InfiniteList(props) {
     children[triggerIndex].ref = elmtRef; 
 
     const handleScroll = (e) => {
-        console.log(e.target)
+        console.log(elmtRef.current.scrollTop)
     }
 
     
