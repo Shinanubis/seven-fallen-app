39de0d8b7bdb9591f4ac4c6ca2b561a502690a51
Mathieu Tourret <pablolola@live.fr>
2021-07-05 11:39:53 +0200

    test fetch

--- src/pages/AddingSubDecksPage.jsx
+++ src/pages/AddingSubDecksPage.jsx
@@ -85,7 +85,7 @@ function AddingSubDecksCardsPage(props) {
                     dataLength={cardsResponse.message[1].length}
                     hasMore={true}
                     loader={<h4 className="title">loading...</h4>}
-                    next={() => setPage(prevState => prevState + 1)}
+                    next={() => setPage(page + 1)}
                     scrollableTarget="list"
                 >
                     {cardsResponse.message[1].map(elmt => {
