f3b95bd2a896a86cd88f8562504ee96ae8d06593
Mathieu Tourret <pablolola@live.fr>
2021-07-05 13:01:27 +0200

    test fetch

--- src/api/CardsWareHouse.jsx
+++ src/api/CardsWareHouse.jsx
@@ -92,10 +92,7 @@ async function getEdenCards(page,count,lang){
             'Authorization': process.env.REACT_APP_TOKEN
         }
     }
-    let url = new URL(`https://api.7fallen.ovh/api/cards/all/${lang.toUpperCase()}`);
-    url.searchParams.append('types', '[1,2,3]');
-    url.searchParams.append('card_count', count);
-    url.searchParams.append('page', page);
+    let url = new URL(`https://api.7fallen.ovh/api/cards/all/${lang.toUpperCase()}?types=[1,2,3]&card_count=${count}&page=${page}`);
     let response = await fetch(url,settings);
     let datas = await response.json();
     return {
