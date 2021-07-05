10a23bc027091b7789e890eee4a07125681ffd85
Mathieu Tourret <pablolola@live.fr>
2021-07-05 11:34:06 +0200

    test fetch

--- src/pages/AddingSubDecksPage.jsx
+++ src/pages/AddingSubDecksPage.jsx
@@ -83,7 +83,7 @@ function AddingSubDecksCardsPage(props) {
             <List className="subdeck list__content layout layout__1">
                 <InfiniteScroll 
                     dataLength={cardsResponse.message[1].length}
-                    hasMore={true}
+                    hasMore={false}
                     next={() => setPage(2)}
                     scrollableTarget="list"
                 >
