92980d497f8049706cec67b50c53caf31137e55e
Mathieu Tourret <pablolola@live.fr>
2021-07-05 18:22:07 +0200

    start hoc for infinite scroll

--- src/components/InfiniteList.jsx
+++ src/components/InfiniteList.jsx
@@ -56,9 +56,9 @@ function InfiniteList(props) {
     return (
         <>
             <ul ref={listRef} className="subdeck list__content layout layout__1 mb-2">
+                {isLoading === true ? <img className="loader__image" src={LoaderGif}/> : null}
                 {props.children}
             </ul>
-            {isLoading === true ? <img className="loader__image" src={LoaderGif}/> : null}
         </>
     )
 }
