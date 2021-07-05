0fc1ae33c2aff7a06e82d3e31a7d033a9a658718
Mathieu Tourret <pablolola@live.fr>
2021-07-05 13:09:57 +0200

    change eden fetch

--- src/HOC/withInfiniteScroll.jsx
+++ src/HOC/withInfiniteScroll.jsx
@@ -1,16 +1,19 @@
 import {useEffect, useState, useRef} from 'react';
 
 const withInfiniteScroll = Component => (props) => {
+
+    const { next } = props;
+
     /*state*/
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(true);
 
     /*refs*/
-    let listRef = useref(null);
+    let listRef = useRef(null);
 
 
     useEffect(() => {
-
+        console.log(props.children)
     }, [])
 
     return (
