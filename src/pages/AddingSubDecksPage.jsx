814ebce77584aae5f9a56b610ebe6e3ddc0a321d
Mathieu Tourret <pablolola@live.fr>
2021-07-05 09:58:09 +0200

    test fetch

--- src/pages/AddingSubDecksPage.jsx
+++ src/pages/AddingSubDecksPage.jsx
@@ -78,7 +78,7 @@ function AddingSubDecksCardsPage(props) {
     return loaded === true ? (
         <Layout>
             <Filters containerClasses="filter__container row  justify-end mb-3" />
-            <p>{test}</p>
+            <p>{test.x}</p>
             <List classes="subdeck list__content layout layout__1">
                 {
                     cardsResponse.message[1].map(elmt => {
