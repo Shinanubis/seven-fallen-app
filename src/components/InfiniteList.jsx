27c0213357d8e4d95eb083df251dcc074099eeb1
Mathieu Tourret <pablolola@live.fr>
2021-07-06 19:54:38 +0200

    test something

--- src/components/InfiniteList.jsx
+++ src/components/InfiniteList.jsx
@@ -50,6 +50,13 @@ function InfiniteList(props) {
     },[isLoading]);
 
     useEffect(() => {
+        
+        if(children.length < numPerPage){
+            console.log("I'm in else if");
+            children[children.length - 1] = elmtRef;
+        }else{
+            console.log("I'm in else");
+        }
 
         setDatas(prevstate => {
             let newDatas = [...prevstate, ...children];
@@ -60,12 +67,7 @@ function InfiniteList(props) {
     useEffect(() => {
         if(children.length === numPerPage){
             children[triggerIndex] = elmtRef;
-        }else if(children.length < numPerPage){
-            console.log("I'm in else if");
-            children[children.length - 1] = elmtRef;
-        }else{
-            console.log("I'm in else");
-        }
+        } 
         window.addEventListener('scroll', handleScroll, true);   
         setIsLoading(false);
         return () => window.removeEventListener('scroll', handleScroll); 
