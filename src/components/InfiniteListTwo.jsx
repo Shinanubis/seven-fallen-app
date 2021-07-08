4a832457086358820e7b6ac62b43d19c568c3521
Mathieu Tourret <pablolola@live.fr>
2021-07-08 09:11:52 +0200

    test infinite scroll

--- src/components/InfiniteListTwo.jsx
+++ src/components/InfiniteListTwo.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState } from 'react';
+import React,{useEffect, useRef, useState } from 'react';
 import LoaderGif from '../img/22-2.gif';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -123,4 +123,4 @@ function InfiniteListTwo(props) {
     )
 }
 
-export default InfiniteListTwo;
+export default React.memo(InfiniteListTwo);
