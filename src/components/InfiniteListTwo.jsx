09e5dde4d0c96d9ac31ca9cf3bd55c867a032047
Mathieu Tourret <pablolola@live.fr>
2021-07-08 09:42:14 +0200

    test infinite scroll

--- src/components/InfiniteListTwo.jsx
+++ src/components/InfiniteListTwo.jsx
@@ -106,11 +106,13 @@ function InfiniteListTwo(props) {
                                         ref={index === triggerIndex ? elmtRef : null} 
                                         className={classesElement ? classesElement : "infinite__element"}
                                     >
+                                        {imagesLoading[elmt.id] &&
                                         <img 
                                             id={elmt.id}
                                             className={classesImages ? classesImages : "infinite__image"} 
                                             src={process.env.REACT_APP_CARDS_STATIC + elmt.image_path}
                                         />
+                                        }
                                         {imagesLoading[elmt.id] === false &&
                                             <img 
                                                 className={classesImages ? classesImages : "infinite__image--loader"}
