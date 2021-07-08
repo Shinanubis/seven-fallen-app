617d59f9929d554c4b0c400a9d8c05a160091bfc
Mathieu Tourret <pablolola@live.fr>
2021-07-08 14:04:41 +0200

    change margin on eden subdeck block

--- src/pages/AddingSubDecksPage.jsx
+++ src/pages/AddingSubDecksPage.jsx
@@ -67,9 +67,7 @@ function AddingSubDecksCardsPage(props) {
 
             if(userSubdeckResponse.code === 200 && completeList instanceof Array){
                 if(userSubdeckResponse.message.length === 0){
-                    newCompleteList.map(elmt => {
-                        return {...elmt, qty: 0}
-                    })
+                    console.log("hello")
                 }
             }
 
