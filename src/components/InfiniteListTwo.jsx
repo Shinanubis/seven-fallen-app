611b1d0acd58cf05934389c67ee201c1de676605
Mathieu Tourret <pablolola@live.fr>
2021-07-10 09:17:57 +0200

    add compoennt will mount

--- src/components/InfiniteListTwo.jsx
+++ src/components/InfiniteListTwo.jsx
@@ -114,7 +114,7 @@ function InfiniteListTwo(props) {
                                             className={classesImages ? classesImages : "infinite__image"} 
                                             src={imagesLoading[elmt.id] === true ? process.env.REACT_APP_CARDS_STATIC + elmt.image_path : LoaderGif}
                                         />
-                                        <CardsCounter value={elmt.qty}/>
+                                        {imagesLoading[elmt.id] === true && <CardsCounter value={elmt.qty}/>}
                                     </li>
                                     )
                                 })
