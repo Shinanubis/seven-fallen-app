3d81f5ccfd17233859492abd6510fa4280536d62
Mathieu Tourret <pablolola@live.fr>
2021-07-10 10:29:22 +0200

    add compoennt will mount

--- src/components/InfiniteListTwo.jsx
+++ src/components/InfiniteListTwo.jsx
@@ -122,7 +122,7 @@ function InfiniteListTwo(props) {
                                 })
                 }
             </ul>
-            {isLoadingList === true && <img className={classesLoaderList ? classesLoaderList : 'infinite__loader--List'} src={LoaderGif}/>}
+            
         </>
     )
 }
