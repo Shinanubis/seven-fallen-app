b6170636f1fa9abf3e3a5fd85e508163c234f76f
Mathieu Tourret <pablolola@live.fr>
2021-07-11 19:02:45 +0200

    change handleImageLoading

--- src/components/InfiniteListTwo.jsx
+++ src/components/InfiniteListTwo.jsx
@@ -118,14 +118,16 @@ function InfiniteListTwo(props) {
                                             src={process.env.REACT_APP_CARDS_STATIC + elmt.image_path} 
                                         />
                                         <CardsCounter 
-                                                classes={imagesLoading[elmt.id] ? "cards__counter" : "d-none"} 
+                                                classes={imagesLoading[elmt.id] === true ? "cards__counter" : "d-none"} 
                                                 value={elmt.qty} 
                                         />
-                                        <img 
-                                            id={`loader__${elmt.id}`}
-                                            className="loader__image"
-                                            src={LoaderGif}  
-                                        />
+                                        {imagesLoading[elmt.id] && 
+                                            <img 
+                                                id={`loader__${elmt.id}`}
+                                                className="loader__image"
+                                                src={LoaderGif}  
+                                            />
+                                        }
                                 </li>
                             )
                         })
