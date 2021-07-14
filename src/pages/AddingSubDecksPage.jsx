9d9792bb9824bc5b984a82079f0a0477ab27f8a9
Mathieu Tourret <pablolola@live.fr>
2021-07-14 15:35:51 +0200

    refacto InfiniteLoader

--- src/pages/AddingSubDecksPage.jsx
+++ src/pages/AddingSubDecksPage.jsx
@@ -66,12 +66,11 @@ function AddingSubDecksCardsPage(props) {
         
         if(response.message[1] instanceof Array && userSubdeckResponse.message[0].cards instanceof Array){
             userSubdeckResponse.message[0].cards.map((elmt, index) => {
-                newCompleteList = [...response.message[1]];
+                newCompleteList = [];
                 response.message[1].map((res, indexRes) => {
+
                     if(elmt[0] === res.id){  
-                        newCompleteList = [...newCompleteList, {...res, qty:elmt[1]}]
-                    }else{
-                        newCompleteList = [...newCompleteList, {...res, qty:0}]
+                        newCompleteList = [...response.message[1], {...res, qty:elmt[1]}]
                     }
                 })
             })
