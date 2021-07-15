a5f91ff2200bc4f7d5429e1b2f703de70366c632
Mathieu Tourret <pablolola@live.fr>
2021-07-15 11:13:04 +0200

    test

--- src/components/InfiniteListThree.jsx
+++ src/components/InfiniteListThree.jsx
@@ -55,10 +55,12 @@ const InfiniteListThree = (props) => {
 
     if(triggerAt < children.props.children.length - 1){
         children.props.children[triggerAt].ref = elmtRef;
+        console.log(children.props.children[triggerAt])
     }
 
-    if(triggerAt >= datas.length - 1){
+    if(triggerAt >= children.props.children.length - 1){
         children.props.children[children.props.children.length - 1].ref = elmtRef;
+        console.log(children.props.children[children.props.children.length - 1])
     }
 
     /*variables*/
