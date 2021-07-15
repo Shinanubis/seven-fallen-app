cf1745ec2d3db5576d178b22b33648ec6b5b69a9
Mathieu Tourret <pablolola@live.fr>
2021-07-15 12:03:53 +0200

    add scroll event to child

--- src/pages/AddingSubDecksPage.jsx
+++ src/pages/AddingSubDecksPage.jsx
@@ -123,7 +123,7 @@ function AddingSubDecksCardsPage(props) {
                 <InfiniteListThree 
                     page={page} 
                     datas={completeList}
-                    triggerAt={6} 
+                    triggerAt={12} 
                     setPage={setPage} 
                     loaderListClasses="loader__image" 
                     loaderList={LoaderGif}
