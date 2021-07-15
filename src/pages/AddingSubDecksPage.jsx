aa7a501d73ec16224414e27548cc896f61be1f87
Mathieu Tourret <pablolola@live.fr>
2021-07-15 11:10:02 +0200

    test

--- src/pages/AddingSubDecksPage.jsx
+++ src/pages/AddingSubDecksPage.jsx
@@ -120,7 +120,14 @@ function AddingSubDecksCardsPage(props) {
                 />
             } */}
             {
-                <InfiniteListThree page={page} datas={completeList} setPage={setPage} loaderListClasses="loader__image" loaderList={LoaderGif}>
+                <InfiniteListThree 
+                    page={page} 
+                    datas={completeList}
+                    triggerAt={5} 
+                    setPage={setPage} 
+                    loaderListClasses="loader__image" 
+                    loaderList={LoaderGif}
+                >
                     <ul className="subdeck list__content layout layout__1 mb-2">
                         <li>salut</li>
                         <li>salut</li>
