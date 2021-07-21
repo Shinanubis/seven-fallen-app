15df656e1bc19de866396454c45df9650ce3ab7e
Devidev <47260520+dev634@users.noreply.github.com>
2021-07-21 04:22:25 +0200

    test

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,15 +2,40 @@ import "./LoginPage.css";
 import React from "react";
 import SocialButton from "../components/SocialButton";
 import wizard from "../img/wizard.jpg";
+import logo from "../img/logos/7-fallen-logo-2.png";
 import dotenv from "dotenv";
 dotenv.config();
 
 function Login(props) {
+	const handleLanguage = (e) => {
+		if (e.target.id === "fr") {
+			console.log("fr");
+		}
+
+		if (e.target.id === "en") {
+			console.log("en");
+		}
+	};
+
 	return (
 		<div
 			className="login page"
 			style={{ backgroundImage: `url(${wizard})` }}
 		>
+			<img className="logo" src={logo} alt="logo de 7 fallen" />
+			<div className="language mb-2">
+				<div
+					id="fr"
+					className="language__choice"
+					onClick={handleLanguage}
+				>
+					FR
+				</div>
+				<span> - </span>
+				<div id="en" className="language__choice">
+					EN
+				</div>
+			</div>
 			<span className="infos mb-1">Se connecter</span>
 			<ul className="social__icons--list">
 				<li className="mb-3">
