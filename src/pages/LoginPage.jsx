c303c16302260f658462c9133c33934c9d7a93ef
Devidev <47260520+dev634@users.noreply.github.com>
2021-07-21 16:27:52 +0200

    add version nuber

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -22,7 +22,7 @@ function Login(props) {
 			className="login page"
 			style={{ backgroundImage: `url(${wizard})` }}
 		>
-			<p className="version">Deckbuilder v1.0.0</p>
+			<p className="version">Deckbuilder v1.0</p>
 			<img className="logo mb-6" src={logo} alt="logo de 7 fallen" />
 			<div className="language mb-2">
 				<div
