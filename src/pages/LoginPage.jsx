66fdf2760519fa91e357d6517a09c8a3e2adb230
Devidev <47260520+dev634@users.noreply.github.com>
2021-07-21 16:47:42 +0200

    test

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -33,28 +33,30 @@ function Login(props) {
 					EN
 				</div>
 			</div>
-			<span className="infos mb-1">Se connecter</span>
-			<ul className="social__icons--list">
-				<li className="mb-3">
-					<SocialButton
-						bgcolor="#395693"
-						url="https://test-seven.site/api/auth/facebook"
-						classes="btn btn__social rounded"
-					>
-						<span className="btn__social--text">Facebook</span>
-					</SocialButton>
-				</li>
-				<li>
-					<SocialButton
-						bgcolor="#F7F7F7"
-						url="https://test-seven.site/api/auth/google"
-						color="#606060"
-						classes="btn btn__social rounded"
-					>
-						<span className="btn__social--text">Google</span>
-					</SocialButton>
-				</li>
-			</ul>
+			<div>
+				<span className="infos mb-1">Se connecter</span>
+				<ul className="social__icons--list">
+					<li className="mb-3">
+						<SocialButton
+							bgcolor="#395693"
+							url="https://test-seven.site/api/auth/facebook"
+							classes="btn btn__social rounded"
+						>
+							<span className="btn__social--text">Facebook</span>
+						</SocialButton>
+					</li>
+					<li>
+						<SocialButton
+							bgcolor="#F7F7F7"
+							url="https://test-seven.site/api/auth/google"
+							color="#606060"
+							classes="btn btn__social rounded"
+						>
+							<span className="btn__social--text">Google</span>
+						</SocialButton>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
