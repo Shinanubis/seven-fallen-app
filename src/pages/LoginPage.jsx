bafc1232f51ab6bc945811a1e48be42c1142c01e
Devidev <47260520+dev634@users.noreply.github.com>
2021-07-21 03:35:06 +0200

    start login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,27 +8,29 @@ dotenv.config();
 
 function Login(props) {
 	return (
-		<ul className="social__icons--list">
-			<li className="mb-3">
-				<SocialButton
-					bgcolor="#395693"
-					url="https://test-seven.site/api/auth/facebook"
-					classes="btn btn__social rounded"
-				>
-					<span className="btn__social--text">Facebook</span>
-				</SocialButton>
-			</li>
-			<li>
-				<SocialButton
-					bgcolor="#F7F7F7"
-					url="https://test-seven.site/api/auth/google"
-					color="#606060"
-					classes="btn btn__social rounded"
-				>
-					<span className="btn__social--text">Google</span>
-				</SocialButton>
-			</li>
-		</ul>
+		<div clasName="login page">
+			<ul className="social__icons--list">
+				<li className="mb-3">
+					<SocialButton
+						bgcolor="#395693"
+						url="https://test-seven.site/api/auth/facebook"
+						classes="btn btn__social rounded"
+					>
+						<span className="btn__social--text">Facebook</span>
+					</SocialButton>
+				</li>
+				<li>
+					<SocialButton
+						bgcolor="#F7F7F7"
+						url="https://test-seven.site/api/auth/google"
+						color="#606060"
+						classes="btn btn__social rounded"
+					>
+						<span className="btn__social--text">Google</span>
+					</SocialButton>
+				</li>
+			</ul>
+		</div>
 	);
 }
 
