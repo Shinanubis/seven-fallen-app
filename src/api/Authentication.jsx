ea3d2002c0200394b16b02c49921685808e2a454
Mathieu Tourret <pablolola@live.fr>
2021-07-24 23:44:12 +0200

    set authentication

--- src/api/Authentication.jsx
+++ src/api/Authentication.jsx
@@ -2,7 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 async function getAuthUser(){
-    let response = await fetch(process.env.REACT_APP_BASE_URL + "/auth/user");
+    let response = await fetch(process.env.REACT_APP_BASE_URL + "/auth/user",{
+        method: 'GET',
+        credentials: "include" 
+    });
     let data = await response.json();
     return data;
 }
