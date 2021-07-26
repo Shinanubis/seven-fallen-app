bcf1e5c189d68b0351899200695d515f48f0823a
Mathieu Tourret <pablolola@live.fr>
2021-07-26 18:52:56 +0200

    built the Authneticated routes componentfdfde

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
 			let extensions = await getExtensionsList("FR");
 			sessionStorage.setItem("extensions", JSON.stringify(extensions));
 		}
-		console.log(isAuthenticated && loaded)
+		console.log("App 1 :",isAuthenticated && loaded)
 		
 	}, []);
 
