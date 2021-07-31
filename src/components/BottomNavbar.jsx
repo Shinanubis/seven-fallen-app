01a9df2a107749a7e2d50dd1ddf4d75e5bc722ea
Mathieu Tourret <pablolola@live.fr>
2021-07-31 16:57:13 +0200

    test

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -3,31 +3,31 @@ import { Link } from 'react-router-dom';
 
 function BottomNavbar(props) {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/profile">
-                        <IoSettingsOutline />
+        <nav className="navbar">
+            <ul className="menu">
+                <li className="menu__element">
+                    <Link className="menu__link" to="/profile">
+                        <IoSettingsOutline className="icon"/>
                     </Link>
                 </li>
                 <li>
                     <Link to="/decks">
-                        <GrMenu />
+                        <GrMenu className="icon"/>
                     </Link>
                 </li>
                 <li>
                     <Link to="/decks/create">
-                        <AiOutlinePlusCircle />
+                        <AiOutlinePlusCircle className="icon"/>
                     </Link>
                 </li>
                 <li>
                     <Link to="/cards">
-                        <BiGridAlt />
+                        <BiGridAlt className="icon"/>
                     </Link>
                 </li>
                 <li>
                     <Link to="/gamers">
-                        <HiOutlineUsers />
+                        <HiOutlineUsers className="icon"/>
                     </Link>
                 </li>
             </ul>
