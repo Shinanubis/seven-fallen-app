be6ffb249875c32332b05ba889d0ca5b472821fd
Mathieu Tourret <pablolola@live.fr>
2021-08-01 15:12:34 +0200

    change decks page style

--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -2,7 +2,7 @@ function PageContainer(props) {
     const {classes} = props;
     return (
         <div className={classes ? `page ${classes}` : "page"}>
-            
+            {props.children}
         </div>
     )
 }
