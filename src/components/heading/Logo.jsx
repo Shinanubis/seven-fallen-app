a5d4c04e4c42f57ed59f442be1a288e8973c7bec
Mathieu Tourret <pablolola@live.fr>
2021-08-02 18:48:22 +0200

    create component and sub component for header

--- src/components/heading/Logo.jsx
+++ src/components/heading/Logo.jsx
@@ -1,15 +1,7 @@
 import LogoFallen from "../../img/logos/7-fallen-logo-2.png";
 
 function Logo(url, alt) {
-    return (
-        <>
-            {url ?
-                <img className="heading__logo" src={url} alt={alt ? alt : "no alt text"} />
-                :
-                <img className="heading__logo" src={`${LogoFallen}`} alt={alt ? alt : "no alt text"} />
-            }
-        </>
-    )
+    return <img className="heading__logo" src={url} alt={alt ? alt : "no alt text"} />
 }
 
 export default Logo;
